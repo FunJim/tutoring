3a54d435858c6c95783bde24c3361e96016f989e
Xunjin ZHENG <xunjin.zheng@ibm.com>
2020-10-30 00:14:25 +0100

    TP2

---
+++ tp2/YujieHanoi_vision.py
@@ -0,0 +1,33 @@
+﻿from hanoi_vision import*
+
+Tour = [[3,2,1],[],[]]
+v=Visualisation_hanoi(Tour)
+
+def bouge(x,y):
+    if len(Tour[x]) == 0:
+        print("La tour de départ est vide")
+    elif len(Tour[y]) == 0:
+        pion = Tour[x][-1]
+        Tour[x].remove(pion)
+        Tour[y].append(pion)
+    else:
+        pion = Tour[x][-1]
+        if Tour[y][-1] > pion:
+            Tour[y].append(pion)
+            Tour[x].remove(pion)
+        else:
+            print("pion pas accepte")
+##bouge(0,2)
+print(Tour)
+
+
+def deplace2pions(depart,arrive,intermediaire):
+    bouge(depart,intermediaire)
+    bouge(depart,arrive)
+    bouge(intermediaire,arrive)
+
+
+deplace2pions(0,2,1)
+
+
+v.mise_a_jour(Tour)
